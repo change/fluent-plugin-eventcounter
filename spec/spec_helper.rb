c35e08bbe0cda1d25a939cfa1dddfc6f3d286098
Hiroshi Hatake <hatake@clear-code.com>
2016-12-07 12:42:36 +0900

    Suppress untouched argument message

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,5 +7,7 @@ Bundler.require(:default, :test)
 require 'fluent/test'
 require 'rspec'
 
+Test::Unit::AutoRunner.need_auto_run = false if defined?(Test::Unit::AutoRunner)
+
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 require 'fluent/plugin/out_eventcounter'
